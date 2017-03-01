5c40b380403a4991d722f6416879dabbea5c2f7b
pandada8 <pandada8@gmail.com>
2017-03-01 23:34:40 +0800

    disable user select

--- src/Pano.vue
+++ src/Pano.vue
@@ -476,6 +476,12 @@ export default {
   cursor: grab;
   cursor: -moz-grab;
   cursor: -webkit-grab;
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+     -moz-user-select: none; /* Firefox */
+      -ms-user-select: none; /* Internet Explorer/Edge */
+          user-select: none; /* Non-prefixed version, currently
+                                supported by Chrome and Opera */
 }
 
 .viewport:active {
